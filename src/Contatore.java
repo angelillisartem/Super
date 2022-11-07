a73cec0741d1a6d51b56e0db97996b1d0e02b9c4
Angelillis Artem <angelillis.artem@osdbsesto>
2022-11-07 12:54:12 +0100

    commit 1

---
+++ src/Contatore.java
@@ -0,0 +1,33 @@
+public class Contatore {
+    private double passo;
+    private double valore;
+
+
+    public Contatore(double passo, double valore) {
+        this.passo = passo;
+        this.valore = valore;
+    }
+
+    public double getPasso() {
+        return passo;
+    }
+    public double getValore() {
+        return valore;
+    }
+    public void setPasso(double passo) {
+        this.passo = passo;
+    }
+    public void setValore(double valore) {
+        this.valore = valore;
+    }
+
+    public double contatore (double x, double y) {
+        passo = x;
+        valore = y;
+    }
+
+    public void resetta () {
+
+    }
+    public
+}
